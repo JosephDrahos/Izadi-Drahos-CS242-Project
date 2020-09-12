d40aa58fca3e692d4864c3963c19d61d4480cbc2
IzadiB <IzadiB@REH-CNU1503G8Y.admin.newpaltz.edu>
2020-09-11 21:21:23 -0400

    Okay so basically More JavaDoc with less milky

--- src/data/FileClackData.java
+++ src/data/FileClackData.java
@@ -1,60 +1,97 @@
 package data;
-
+/**
+ * Represents data from ClackData as the name and contents of a file
+ * Inherits variables and methods from ClackData
+ * @author Joseph Drahos & Rod Izadi
+ *
+ */
 public class FileClackData extends ClackData{
-	private String fileName;
-	private String fileContents;
+	private String fileName;		//String representing name of file
+	private String fileContents;	//String representing contents of file
 	
+	/**
+	 * Constructor to set up Username, File Name, and type
+	 * @param userName inherited from ClackData
+	 * @param fileName: name of file(Automatically set to Null)
+	 * @param type: inherited from ClackData
+	 */
 	public FileClackData(String userName, String fileName, int type){
 		super(userName, type);
 		this.fileName = fileName;
 		this.fileContents = null;
 	}
 	
+	/**
+	 * Overloaded previous constructor with fileName set to Null as well as fileContents
+	 */
 	public FileClackData(){
 		super();
 		this.fileName = null;
 		this.fileContents = null;
 	}
 	
+	/**
+	 * Sets fileName
+	 * @param fileName:Name of file
+	 */
 	public void setFileName(String fileName){
 		this.fileName = fileName;
 	}
 	
+	/**
+	 * 
+	 * @returns Name of file
+	 */
 	public String getFileName() {
 		return this.fileName;
 	}
-	
+	/**
+	 * @returns file Contents
+	 */
 	@Override
 	public String getData() {
 		return this.fileContents;
 	}
 	
+	/**
+	 * Reads File Contents
+	 * WILL IMPLEMENT LATER
+	 */
 	public void readFileContents() {
 		
 	}
 	
+	/**
+	 * Writes File Contents
+	 * WILL IMPLEMENT LATER
+	 */
 	public void writeFileContents() {
 		
 	}
 	
+	/**
+	 * Overrides default hashCode function to give unique hashCode for every fileClackData object
+	 * @returns hashCode number
+	 */
 	@Override
 	public int hashCode() {
-		int hash = 1;
-		String user = super.userName;
-		
-		for(int i = 0; i < user.length(); i++) {
-			hash = hash * (int) user.charAt(i);
-		}
-		
-		return hash;
+		return (int) ((Math.random() * ( 300000-1)) + 1);
 	}
 	
+	/**
+	 * Overrides default equals method and compares all variables in a fileClackData object to check equivalence
+	 * @returns true or false
+	 */
 	@Override
 	public boolean equals(Object other) {
 		FileClackData otherData = (FileClackData) other;
 		return this.userName == otherData.userName && this.getType() == otherData.getType() && this.fileName == otherData.fileName && this.fileContents == otherData.fileContents;
 	}
 	
+	/**
+	 * Overrides toString method to outputs strings of all fileClackData data
+	 * returns string of object data
+	 */
 	@Override
 	public String toString() {
 		String output;
--- src/data/MessageClackData.java
+++ src/data/MessageClackData.java
@@ -1,42 +1,64 @@
 package data;
 
-
+/**
+ * Represents data from ClackData as a message
+ * inherits ClackData variables and methods
+ * @author Rod Izadi & Joseph Drahos
+ *
+ */
 public class MessageClackData extends ClackData{
-	private String message;
+	private String message;	//String representing instant message
 	
+	/**
+	 * Constructor which calls super constructor from ClackData
+	 * @param userName: inherited from ClackData
+	 * @param message: inherited from ClackData
+	 * @param type: inherited from ClackData
+	 */
 	public MessageClackData(String userName, String message, int type){
 		super(userName, type);
 		this.message = message;
 	}
 	
+	/**
+	 * Overloaded previous method with no arguments
+	 */
 	public MessageClackData(){
 		super();
 		this.message = null;
 	}
 	
+	/**
+	 * @returns message data
+	 */
 	@Override
 	public String getData() {
 		return this.message;
 	}
 	
+	/**
+	 * Overrides default hashCode function to give unique hashCode for every messageClackData object
+	 * @returns hashCode number
+	 */
 	@Override
 	public int hashCode() {
-		int hash = 1;
-		String user = super.userName;
-		
-		for(int i = 0; i < user.length(); i++) {
-			hash = hash * (int) user.charAt(i);
-		}
-		
-		return hash;
+		return (int) ((Math.random() * ( 300000-1)) + 1);
 	}
 	
+	/**
+	 * Overrides default equals method and compares all variables in a messageClackData object to check equivalence
+	 * @returns true or false
+	 */
 	@Override
 	public boolean equals(Object other) {
 		MessageClackData otherData = (MessageClackData) other;
 		return this.message == otherData.getData() && this.userName == otherData.userName && this.getType() == otherData.getType();
 	}
 	
+	/**
+	 * Overrides toString method to outputs strings of all messageClackData data
+	 * returns string of object data
+	 */
 	@Override
 	public String toString() {
 		String output;
